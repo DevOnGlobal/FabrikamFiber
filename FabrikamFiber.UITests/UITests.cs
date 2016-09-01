5f4f86a9f0cb2f7f427df5f468eb9ab469a5325a
Harm Pauw <h.pauw@prowareness.nl>
2016-09-01 20:51:42 +0200

    UI + unit tests

---
+++ FabrikamFiber.UITests/UITests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium;
+
+namespace FabrikamFiber.UITests
+{
+    [TestClass]
+    public class UITests
+    {
+        public TestContext TestContext
+        {
+            get; set;
+        }
+
+        [TestMethod]
+        public void TestHomepage()
+        {
+            IWebDriver driver;
+            if (!string.IsNullOrEmpty(TestContext.Properties["SELENIUM_GRID_URL"] as string)) {
+                driver = new OpenQA.Selenium.Remote.RemoteWebDriver(new Uri(TestContext.Properties["SELENIUM_GRID_URL"] as string), DesiredCapabilities.Chrome());
+            } else
+            {
+                driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+            }
+
+            try { 
+                driver.Navigate().GoToUrl(TestContext.Properties["APP_URL"] as string);
+                Assert.IsTrue(driver.PageSource.Contains("Dashboard"));
+            } finally { 
+                driver.Quit();
+            }
+        }
+    }
+}
